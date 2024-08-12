d8dd2f406d0fb7b859561c2f7aefc93befc9349c
ArthurSimplicio <arthurpark20@gmail.com>
2024-08-12 07:51:52 -0300

    adding content and updating styles

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const menu = document.querySelector('.menu')
-        const navMenu = document.querySelector('.nav-menu')
-        menu.addEventListener('click', () => {
-            menu.classList.toggle('active')
-            navMenu.classList.toggle('active')
-        })
+const navMenu = document.querySelector('.nav-menu')
+menu.addEventListener('click', () => {
+    menu.classList.toggle('active')
+    navMenu.classList.toggle('active')
+})
+const about = document.querySelector('.about1')
+const aboutScroll = document.querySelector('#about')
+about.addEventListener('click', () => about.scrollTo(aboutScroll))
+
